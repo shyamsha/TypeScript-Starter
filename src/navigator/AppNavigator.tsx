a7085519b13d00f66cc3114375af37cd2100f788
Syam Kumar <shyamkumar97@live.com>
2019-12-19 18:27:59 +0530

    set up linting and pre commit hook

---
+++ src/navigator/AppNavigator.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AppNavigator() {
+  return <div></div>;
+}
--- src/services/Api.ts
+++ src/services/Api.ts
@@ -2,6 +2,7 @@ import config from "../config/app";
 
 const isProd: boolean = config.isProd;
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const API_ENDPOINT = isProd
   ? config.production.api_endpoint
-  : config.staging.api_endpoint
+  : config.staging.api_endpoint;
